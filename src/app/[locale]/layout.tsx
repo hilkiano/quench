9be6262d709a44109708c762303a4b479b6b7975
hilkiano <hilkia.nohansen@gmail.com>
2025-01-30 11:53:53 +0700

    remove hidden overflow

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -65,7 +65,7 @@ export default async function RootLayout({
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body
-        className={`${fredokaFont.variable} ${zzzFont.variable} ${mulishFont.variable} font-sans antialiased bg-[url('/images/bg.svg')] bg-cover bg-no-repeat bg-center bg-fixed overscroll-none overflow-hidden`}
+        className={`${fredokaFont.variable} ${zzzFont.variable} ${mulishFont.variable} font-sans antialiased bg-[url('/images/bg.svg')] bg-cover bg-no-repeat bg-center bg-fixed overscroll-none`}
       >
         <Toaster />
         <QueryProvider>
